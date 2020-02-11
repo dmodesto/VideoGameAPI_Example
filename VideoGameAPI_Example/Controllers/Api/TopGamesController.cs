653898e3195a044ff67950fda4563897e5a29cbb
Dan Modesto <danmodesto76@gmail.com>
2020-02-11 13:27:00 -0600

    Added structure for TopGames and Search functionality.

---
+++ VideoGameAPI_Example/Controllers/Api/TopGamesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VideoGameAPI_Example.Controllers.Api
+{
+    public class TopGamesController : ApiController
+    {
+        public IHttpActionResult Index()
+        {
+            return null;
+        }
+    }
+}
