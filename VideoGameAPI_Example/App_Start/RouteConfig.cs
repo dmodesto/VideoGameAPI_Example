c5907d4c5e1335f2bc094abb8ec32f2c9a14c333
Dan Modesto <danmodesto76@gmail.com>
2020-02-11 12:36:21 -0600

    Set Route config to use Attribute routing.

--- VideoGameAPI_Example/App_Start/RouteConfig.cs
+++ VideoGameAPI_Example/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace VideoGameAPI_Example
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
