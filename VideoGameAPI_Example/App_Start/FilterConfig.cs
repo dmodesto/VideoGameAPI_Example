7d65a145d122775a5c3f7d1b149d9e7fd15b3840
Dan Modesto <danmodesto76@gmail.com>
2020-02-14 11:16:09 -0600

    Revert "Set project to use SSL, and updated FilterConfig to only allow HTTPS connections."
    This reverts commit 00deacc18eddb36bdbfc52227bcc99404ba09e50.

--- VideoGameAPI_Example/App_Start/FilterConfig.cs
+++ VideoGameAPI_Example/App_Start/FilterConfig.cs
@@ -8,7 +8,6 @@ namespace VideoGameAPI_Example
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
