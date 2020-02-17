60f6112136d02abd44db79a1a0ea9d011fcb9913
Dan Modesto <danmodesto76@gmail.com>
2020-02-17 07:24:09 -0600

    Added RestSharp and DataTables to easily use the IGDB api. Consolidated some JavaScript bundles. Created the first api call to IGDB.

--- VideoGameAPI_Example/Controllers/Api/TopGamesController.cs
+++ VideoGameAPI_Example/Controllers/Api/TopGamesController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Serialization.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,9 +13,24 @@ namespace VideoGameAPI_Example.Controllers.Api
 {
     public class TopGamesController : ApiController
     {
+        // GET
+        [HttpGet]
         public IHttpActionResult Index()
         {
-            return null;
+            var client = new RestClient(Properties.Settings.Default.igdbUrl + "games");                        
+            client.Timeout = -1;
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("user-key", Properties.Settings.Default.userKey);
+            request.AddHeader("Content-Type", "text/plain");
+
+            // Todo: use passed parameters to build the query string
+            request.AddParameter("text/plain", "fields *; where platforms = [165] & total_rating >= 75; sort total_rating desc; limit 25;", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            dynamic jsonResponse = JsonConvert.DeserializeObject(response.Content);
+
+            return Json(jsonResponse);
         }
     }
 }
